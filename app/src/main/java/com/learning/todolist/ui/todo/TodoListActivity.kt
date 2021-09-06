be1d9029c5c0dc8de2fa97210c255977439ee16a
nagarro-kunalmadan <kunal.madan@nagarro.com>
2021-09-06 17:03:56 +0530

    Basic package structure.

--- app/src/main/java/com/learning/todolist/ui/todo/TodoListActivity.kt
+++ app/src/main/java/com/learning/todolist/ui/todo/TodoListActivity.kt
@@ -1,9 +1,10 @@
-package com.learning.todolist
+package com.learning.todolist.ui.todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.learning.todolist.R
 
-class MainActivity : AppCompatActivity() {
+class TodoListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/learning/todolist/ui/todo/TodoListViewModel.kt
@@ -0,0 +1,4 @@
+package com.learning.todolist.ui.todo
+
+class TodoListViewModel {
+}
