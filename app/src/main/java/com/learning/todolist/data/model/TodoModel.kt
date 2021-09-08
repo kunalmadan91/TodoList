9137d46647a6ecc72aaf4405cc72f2de2124cd5a
nagarro-kunalmadan <kunal.madan@nagarro.com>
2021-09-08 14:53:49 +0530

    Added model

---
+++ app/src/main/java/com/learning/todolist/data/model/TodoModel.kt
@@ -0,0 +1,13 @@
+package com.learning.todolist.data.model
+import com.google.gson.annotations.SerializedName
+
+data class TodoModel(
+    @SerializedName("completed")
+    val completed: Boolean?,
+    @SerializedName("id")
+    val id: Int?,
+    @SerializedName("title")
+    val title: String?,
+    @SerializedName("userId")
+    val userId: Int?
+)
